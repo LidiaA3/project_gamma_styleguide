921774c3e17ea61dd81060d97272ae8a2f7ea8d8
Lidia A <lidia.lasy@gmail.com>
2023-05-10 20:18:37 +0200

    Add typography structure and utilities

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,9 @@ import './Button.scss'
 function Button() {
     return (
         <>
-            <button className='btn' type="button">Name</button>
+            <button className='btn' type="button">Primary</button>
+            <button className='btn btn--secondary' type="button">Secondary</button>
+            <button className='btn btn--cta' type="button">CTA</button>
         </>
     );
 }
--- src/styleguide/Styleguide.jsx
+++ src/styleguide/Styleguide.jsx
@@ -16,39 +16,41 @@ function Styleguide() {
           <h4>Así se ve un h4</h4>
           <h5>Así se ve un h5</h5>
           <h6>Así se ve un h6</h6>
+          <p>This is a paragraph. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas consequatur ad odit ea asperiores laudantium rem optio nostrum officiis totam, et a. Molestiae suscipit maiores nostrum saepe soluta exercitationem repellat.</p>
+          <small>This is an small</small>
         </div>
 
         <div id="colors">
           <div className='color__group'>
             <div className="color__box">
-              <div className="color bg-gray_light"> </div>
+              <div className="color color-bg-gray_light"> </div>
               <p>gray_light</p>
             </div>
             <div className="color__box">
-              <div className="color bg-gray"> </div>
+              <div className="color color-bg-gray"> </div>
               <p>gray</p>
             </div>
             <div className="color__box">
-              <div className="color bg-gray_dark"> </div>
+              <div className="color color-bg-gray_dark"> </div>
               <p>gray_dark</p>
             </div>
             <div className="color__box">
-              <div className="color bg-gray_darken"> </div>
+              <div className="color color-bg-gray_darken"> </div>
               <p>gray_darken</p>
             </div>
           </div>
 
           <div className='color__group'>
             <div className="color__box">
-              <div className="color bg-pistacho"> </div>
+              <div className="color color-bg-pistacho"> </div>
               <p>pistacho</p>
             </div>
             <div className="color__box">
-              <div className="color bg-violet"> </div>
+              <div className="color color-bg-violet"> </div>
               <p>violet</p>
             </div>
             <div className="color__box">
-              <div className="color bg-purple"> </div>
+              <div className="color color-bg-purple"> </div>
               <p>purple</p>
             </div>
           </div>
