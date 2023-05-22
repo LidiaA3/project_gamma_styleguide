d061e5afd3dabbcef09820e9776b7a03ee6f0ca7
Lidia A <lidia.lasy@gmail.com>
2023-05-22 17:36:46 +0200

    Button styled

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -6,11 +6,15 @@ function Button() {
         <>
             <button className='btn' type="button">Primary</button>
             <button className='btn btn--secondary' type="button">Secondary</button>
-            <button className='btn btn--cta' type="button">CTA</button>
+            <button className='btn btn--x-big' type="button">CTA</button>
             <hr />
-            <Link className='btn'>Primary</Link>
-            <Link className='btn btn--secondary'>Secondary</Link>
-            <Link className='btn btn--cta'>CTA</Link>
+            <button className='btn' disabled>Primary</button>
+            <button className='btn btn--secondary' disabled>Secondary</button>
+            <button className='btn btn--small'>CTA</button>
+            <hr />
+            <Link className='btn' disabled>Primary</Link>
+            <Link className='btn btn--secondary' disabled>Secondary</Link>
+            <Link className='btn btn--small'>CTA</Link>
         </>
     );
 }
