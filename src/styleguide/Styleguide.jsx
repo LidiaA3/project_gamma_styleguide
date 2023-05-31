7b6c7cbfd978889de5b1dbfc5e7185652d23b030
Lidia A <lidia.lasy@gmail.com>
2023-05-31 20:59:26 +0200

    Add styleguide refactor

--- src/styleguide/Styleguide.jsx
+++ src/styleguide/Styleguide.jsx
@@ -4,6 +4,7 @@ import Button from '../components/button/Button'
 import StyColorCard from './StyColorCard'
 import IconEmail from '../components/icons/IconEmail'
 import Accordion from '../components/accordion/Accordion'
+import IconHome from './icons/IconHome'
 
 function Styleguide() {
 
@@ -95,11 +96,12 @@ function Styleguide() {
         <article id='buttons'>
           <h3>Buttons and Links</h3>
           <section className="stySection">
-            <p>General attributes <code>type=''|'secondary' status=''|'disable' isLink=true|false</code></p>
+            <p>General attributes <code>type=''|'secondary'|'ternary' status=''|'disable' isLink=true|false</code></p>
             <div className="stySection__item">
               <p>Others <code>size='x-big'</code></p>
               <Button text='Email' icon={<IconEmail/>} type='primary' size='x-big' />
               <Button text='Email' icon={<IconEmail/>} type='secondary' size='x-big' />
+              <Button text='Email' icon={<IconEmail/>} type='ternary' size='x-big' />
               <Button text='Email' icon={<IconEmail/>} type='primary' size='x-big' status='disable' />
               <Button text='Email' icon={<IconEmail/>} type='secondary' size='x-big' status='disable' />
             </div>
@@ -107,6 +109,7 @@ function Styleguide() {
               <p>Others <code>size='big'</code></p>
               <Button text='Email' icon={<IconEmail/>} type='primary' size='big' />
               <Button text='Email' icon={<IconEmail/>} type='secondary' size='big' />
+              <Button text='Email' icon={<IconEmail/>} type='ternary' size='big' />
               <Button text='Email' icon={<IconEmail/>} type='primary' size='big' status='disable' />
               <Button text='Email' icon={<IconEmail/>} type='secondary' size='big' status='disable' />
             </div>
@@ -114,6 +117,7 @@ function Styleguide() {
               <p>Others <code>size=''</code></p>
               <Button text='Email' icon={<IconEmail/>} type='primary' size='' />
               <Button text='Email' icon={<IconEmail/>} type='secondary' size='' />
+              <Button text='Email' icon={<IconEmail/>} type='ternary' size='' />
               <Button text='Email' icon={<IconEmail/>} type='primary' size='' status='disable' />
               <Button text='Email' icon={<IconEmail/>} type='secondary' size='' status='disable' />
             </div>
@@ -121,31 +125,191 @@ function Styleguide() {
               <p>Others <code>size='small'</code></p>
               <Button text='Email' icon={<IconEmail/>} type='primary' size='small' />
               <Button text='Email' icon={<IconEmail/>} type='secondary' size='small' />
+              <Button text='Email' icon={<IconEmail/>} type='ternary' size='small' />
               <Button text='Email' icon={<IconEmail/>} type='primary' size='small' status='disable' />
               <Button text='Email' icon={<IconEmail/>} type='secondary' size='small' status='disable' />
             </div>
-            <p>Links <code>isLink=true</code> for size <code>btn--$size</code> if secondary <code>type='secondary'</code></p>
+            <p>Links <code>isLink=true</code> for size <code>btn--$size</code> if secondary or ternary <code>type='secondary'|'ternary'</code> Target of link <code>target=''|'_blank'</code></p>
             <div className="stySection__item">
-              <Button text='Email link' icon={<IconEmail/>} type='primary' size='small' />
-              <Button text='Email link' icon={<IconEmail/>} type='secondary' />
+              <Button isLink={true} text='Email link' icon={<IconEmail/>} type='primary' size='small' />
+              <Button isLink={true} text='Email link' icon={<IconEmail/>} type='secondary' />
+              <Button isLink={true} text='Email link' icon={<IconEmail/>} type='ternary' size='small' />
             </div>
             <p>Parameters for buttons <code>text | icon | type | size | status | handleClick</code></p>
-            <p>Parameters for links <code>text | icon | type | size | isLink | goTo</code></p>
+            <p>Parameters for links <code>text | icon | type | size | isLink | goTo | target</code></p>
           </section>
         </article>
 
         <article>
-          <Accordion tittle='Accordion component tittle with image' img='http://via.placeholder.com/400x300' text='Lorem ipsum dolor sit amet consectetur adipiscing elit lobortis ante sollicitudin habitasse ligula morbi dictumst, litora nisl sapien semper tortor ridiculus fusce blandit class mus maecenas venenatis nascetur. Rhoncus metus eget purus ridiculus pulvinar dictumst ut per erat penatibus aenean, tempor fusce nam sapien himenaeos donec fringilla in interdum facilisi, lacus lobortis semper suspendisse inceptos nisl laoreet dictum vel phasellus.' />
-          <Accordion tittle='Accordion component tittle' text='Lorem ipsum dolor sit amet consectetur adipiscing elit lobortis ante sollicitudin habitasse ligula morbi dictumst, litora nisl sapien semper tortor ridiculus fusce blandit class mus maecenas venenatis nascetur. Rhoncus metus eget purus ridiculus pulvinar dictumst ut per erat penatibus aenean, tempor fusce nam sapien himenaeos donec fringilla in interdum facilisi, lacus lobortis semper suspendisse inceptos nisl laoreet dictum vel phasellus.' />
-          <Accordion tittle='Accordion component tittle' text='Lorem ipsum dolor sit amet consectetur adipiscing elit lobortis ante sollicitudin habitasse ligula morbi dictumst, litora nisl sapien semper tortor ridiculus fusce blandit class mus maecenas venenatis nascetur. Rhoncus metus eget purus ridiculus pulvinar dictumst ut per erat penatibus aenean, tempor fusce nam sapien himenaeos donec fringilla in interdum facilisi, lacus lobortis semper suspendisse inceptos nisl laoreet dictum vel phasellus.' />
+          <h4>Accordion component</h4>
+          <code>&lt;Accordion/&gt; properties: tittle | img(optional) | text</code>
+          <Accordion tittle='Accordion component with image tittle' img='http://via.placeholder.com/400x300' text='Lorem ipsum dolor sit amet consectetur adipiscing elit lobortis ante sollicitudin habitasse ligula morbi dictumst, litora nisl sapien semper tortor ridiculus fusce blandit class mus maecenas venenatis nascetur. Rhoncus metus eget purus ridiculus pulvinar dictumst ut per erat penatibus aenean, tempor fusce nam sapien himenaeos donec fringilla in interdum facilisi, lacus lobortis semper suspendisse inceptos nisl laoreet dictum vel phasellus.' />
+          <Accordion tittle='Accordion component without image tittle' text='Lorem ipsum dolor sit amet consectetur adipiscing elit lobortis ante sollicitudin habitasse ligula morbi dictumst, litora nisl sapien semper tortor ridiculus fusce blandit class mus maecenas venenatis nascetur. Rhoncus metus eget purus ridiculus pulvinar dictumst ut per erat penatibus aenean, tempor fusce nam sapien himenaeos donec fringilla in interdum facilisi, lacus lobortis semper suspendisse inceptos nisl laoreet dictum vel phasellus.' />
         </article>
 
         <article id="form">
           <h3>Form assets</h3>
-          <input type="text" />
-          <input type="button" />
-          <input type="checkbox" />
-          <input type="radio" />
+          <h4>Text inputs</h4>
+            <div className="inputgroup">
+                <label htmlFor="text">Short text:
+                <input id="text" type="text" placeholder="Write something" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="l-text">Large text:
+                <textarea id="l-text" name="" cols="20" rows="3" placeholder="This is a large text where you could type whatever."></textarea>
+                </label>
+            </div>
+            
+            <div className="inputgroup">
+                <label htmlFor="email">Email:
+                <input type="email" name="" id="email" placeholder="ejemplo@ejemplo.com" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="password">Password:
+                <input type="password" name="" id="password" placeholder="Set your password" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="search">Search:
+                <input type="search" name="" id="search" placeholder="Search something" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="url">Set an URL:
+                <input type="url" name="" id="url" placeholder="https://www.google.es/"/>
+                </label>
+            </div>
+
+
+          <h4>Number inputs</h4>
+            <div className="inputgroup">
+                <label htmlFor="number">Set a number:
+                <input type="number" name="" id="number" placeholder="5382" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="tel">Set your phone:
+                <input type="tel" name="" id="tel" placeholder="628 657 426" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="date">Set a date:
+                <input type="date" name="" id="date" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="time">Set a time:
+                <input type="time" name="" id="time" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="datetime-local">Set date and time:
+                <input type="datetime-local" name="" id="datetime-local" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="week">Set a week:
+                <input type="week" name="" id="week" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="month">Set a month:
+                <input type="month" name="" id="month" />
+                </label>
+            </div>
+
+
+          <h4>Select inputs</h4>
+            <div className="inputgroup">
+                <label htmlFor="radio1">opt1:
+                <input type="radio" name="opps" id="radio1" />
+                </label>
+                <label htmlFor="radio2">opt2:
+                <input type="radio" name="opps" id="radio2" />
+                </label>
+                <label htmlFor="radio3">opt3:
+                <input type="radio" name="opps" id="radio3" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="checkbox1">check1:
+                <input type="checkbox" name="" id="checkbox1" />
+                </label>
+                <label htmlFor="checkbox2">check2:
+                <input type="checkbox" name="" id="checkbox2" />
+                </label>
+                <label htmlFor="checkbox3">check3:
+                <input type="checkbox" name="" id="checkbox3" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="range">Set a range:
+                <input type="range" name="" id="range" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="color">Set a color:
+                <input type="color" name="" id="color" defaultValue="#851A50" />
+                </label>
+            </div>
+
+            <div className="inputgroup">
+                <label htmlFor="select">Select an option:
+                <select name="" id="select" defaultValue='1'>
+                    <option defaultValue='1' disabled>Select an option</option>
+                    <optgroup label="Option 1:">
+                        <option defaultValue=''>Option 1.1</option>
+                        <option defaultValue=''>Option 1.2</option>
+                        <option defaultValue=''>Option 1.3</option>
+                    </optgroup>
+                    <optgroup label="Option 2:">
+                        <option defaultValue=''>Option 2.1</option>
+                        <option defaultValue=''>Option 2.2</option>
+                        <option defaultValue=''>Option 2.3</option>
+                    </optgroup>
+                </select>
+                </label>
+            </div>
+
+            
+          <h4>File inputs</h4>
+            <div className="inputgroup">
+                <label htmlFor="file">Enter a file:
+                <input type="file" name="" id="file" />
+                </label>
+            </div>
+
+
+          <h4>Button inputs</h4>
+            <div className="inputgroup">
+                <input className="btn" type="submit" defaultValue="submit" />
+                <input className="btn" type="button" defaultValue="button" />
+                <input className="btn" type="reset" defaultValue="reset" />
+                <input className="btn" type="image" id="image" alt="awesome" height="32px" src="icons/awesome.png" />
+            </div>
+
+
+          <h4>Progress state</h4>
+            <div className="inputgroup">
+              <label htmlFor="state">Progress state:
+              <progress id="state" min="0" max="100" value="70" readOnly> 70% </progress>
+              </label>
+            </div>
         </article>
 
         <article id='grid'>
